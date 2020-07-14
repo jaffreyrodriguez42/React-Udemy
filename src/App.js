23d60d1fbca77c1bf2953be6a93a2a20f793f52d
Jaff Rey Rodriguez <jaffreyrodriguez42@gmail.com>
2020-07-14 21:23:41 +0800

    Inline Styling for React Elements

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import './App.css';
 
 function App() {
 
-  const img = "https://picsum.photos/200";
+  const customStyle = {
+    color: "red",
+    fontSize: "20px",
+    border: "1px solid black"
+  }
+
+  customStyle.color = "blue";
+
 
   return (
-    <div>
-      <h1 className="heading" contentEditable="true" spellCheck="false">My Favorite Foods</h1>
-      <img className="food-img" src="https://cdn.pixabay.com/photo/2020/01/17/13/18/sheep-4772994__340.jpg" alt="food-img" />
-      <img className="food-img" src="https://cdn.pixabay.com/photo/2019/10/30/15/33/tajikistan-4589831__340.jpg" alt="food-img" />
-      <img className="food-img" src="https://cdn.pixabay.com/photo/2020/07/08/04/12/work-5382501__340.jpg" alt="food-img" />
-      <img src={img + "?grayscale"} alt="randomImg" />
-    </div>
+    <h1 style={customStyle}>Hello world!</h1>
   );
 }
 
