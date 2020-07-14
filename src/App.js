bcb0e4e57e1b84c68194491227ba0730e08e5273
Jaff Rey Rodriguez <jaffreyrodriguez42@gmail.com>
2020-07-14 15:34:07 +0800

    JSX Attributes & Styling React Elements

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const fName = "Jaff Rey";
-  const dt = new Date();
-  const year = dt.getFullYear();
 
+  const img = "https://picsum.photos/200";
 
   return (
     <div>
-      <p>Created By {fName}.</p>
-      <p>Copyright {year}</p>
+      <h1 className="heading" contentEditable="true" spellCheck="false">My Favorite Foods</h1>
+      <img className="food-img" src="https://cdn.pixabay.com/photo/2020/01/17/13/18/sheep-4772994__340.jpg" alt="food-img" />
+      <img className="food-img" src="https://cdn.pixabay.com/photo/2019/10/30/15/33/tajikistan-4589831__340.jpg" alt="food-img" />
+      <img className="food-img" src="https://cdn.pixabay.com/photo/2020/07/08/04/12/work-5382501__340.jpg" alt="food-img" />
+      <img src={img + "?grayscale"} alt="randomImg" />
     </div>
   );
 }
