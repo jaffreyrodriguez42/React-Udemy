39c095cbed43f24d9c29b12a32cae892172f0717
Jaff Rey Rodriguez <jaffreyrodriguez42@gmail.com>
2020-07-15 11:30:04 +0800

    React Components

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Heading from "./components/Heading";
+import List from "./components//List";
 
 function App() {
-
-  const customStyle = {
-    color: ""
-  }
-
-  let greeting;
-
-  const dt = new Date();
-
-  const currentTime = dt.getHours();
-
-  if (currentTime < 12) {
-    greeting = "Good Morning";
-    customStyle.color = "red";
-  } else if (currentTime < 18) {
-    greeting = "Good Afternoon";
-    customStyle.color = "green";
-  } else {
-    greeting = "Good Night";
-    customStyle.color = "blue";
-  }
-
   return (
-    <h1 className="heading" style={customStyle}>{greeting}</h1>
+    <div>
+      <Heading />
+      <List />
+    </div>
+
   );
 }
 
